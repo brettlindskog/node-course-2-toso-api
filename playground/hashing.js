69aa6ca81021623b6181dd707b8903a3968a646a
Brett Lindskog <blindsko@us.ibm.com>
2018-01-30 14:37:54 -0700

    Has user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,28 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, 'secretsalt');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+hashedPassword = '$2a$10$EEvUqQWZ0X9KVlQ6slpFSe8CS.AYOs8GJ/nKdr8rYm.UEiBJ9CJVy';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
-var decoded = jwt.verify(token, 'secretsalt');
-console.log('decoded: ', decoded);
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, 'secretsalt');
+// console.log(token);
+
+// var decoded = jwt.verify(token, 'secretsalt');
+// console.log('decoded: ', decoded);
 
 // var message = 'I am user number 3';
 
